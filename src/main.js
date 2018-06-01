27d3dd345f00a90220b8f577abc9accf863fad97
CLM <limin_chu@163.com>
2018-06-01 18:29:34 +0800

    calendar
    calendar

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import 'styles/reset.css'
-import 'styles/border.css'
+// import 'styles/reset.css'
+// import 'styles/border.css'
 
 Vue.config.productionTip = false
-fastclick.attach(document.body)
+// fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
