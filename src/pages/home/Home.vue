678cb244aa2b197d4bfd89a9fe2ec5fe488cc9e5
CLM <limin_chu@163.com>
2018-06-01 16:53:23 +0800

    calendar
    calendar

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <home-calendar></home-calendar>
+</div>
+</template>
+
+<script>
+import HomeCalendar from './components/Calendar'
+export default {
+  name: 'Home',
+  components: {
+    HomeCalendar
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/pages/home/components/Calendar.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+HomeCalendar
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeCalendar'
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
